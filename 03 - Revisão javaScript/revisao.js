9d05a08da5780ca5d03e70d949c4e8ecefe9fb85
Matheus Bruno <matheussbsiilva@gmail.com>
2025-03-06 20:25:38 -0300

    atualizaçóes

--- 03 - Revisão javaScript/revisao.js
+++ 03 - Revisão javaScript/revisao.js
@@ -6,6 +6,8 @@ comentário em bloco
 
 //variaveis
 //  var => Náo usa no dia a dia
+
+/*
 let nome = "Matheus";
 const idade = 24;
 
@@ -45,18 +47,84 @@ else {
 
     // arrays - listas
 
-    const carros = ["Uno", "Gol", "Voyage"]
+    const carros = {"Uno", "Gol", "Voyage"}
 
     console.log(carros(0))
     console.log(carros(1)) 
 
-    console.log("Quantas frutas tem no array")
+    console.log("Quantos carros tem no array")
     console.log (carros.length)
 
-    frutas.push("Laranja")
-    console.log("E agora quantas frutas tem no meu array")
+    carros.push("palio")
+    console.log("E agora quantos carros tem no meu array")
     console.log(carros.length)
 
     carros.forEach(carro => console.log(carro))
 
     carros.pop
+    console.log(carros)
+    */
+
+    // objetos
+    let pessoa = {
+        nome: "Gustavo",
+        idade: 30,
+        peso: 80,
+        altura: 170
+    }
+
+    console.log(pessoa.nome)
+
+    pessoa.idade = 40
+    console.log(pessoa.idade)
+
+    // desatribuição de objetos
+
+    const {peso, altura} = pessoa
+    console.log(peso)
+    console.log(altura)
+
+    // propagação de objetos
+
+    const endereco= {
+        cidade: "Brasília",
+        uf:"DF"
+    }
+    const pessoaCompleto = {
+        ...pessoa,
+        ...endereco,
+    }
+    console.log(pessoaCompleto)
+                                                                                                                                                                                                                                                
+    // funções
+    function somar(numA, numB) {
+        const resultado = numA + numB
+        console.log(resultado)
+    }
+
+    // uso
+    somar(2,2)
+    somar(3,3)
+    somar(10,5)
+    
+    // função anonima
+    const subtrair = function (numA, numB){
+        const resultado = numA - numB
+        console.log(resultado)
+    }
+    subtrair(6.2)
+
+    // arrow funtion
+    const multiplicar = (numA, numB) => numA * numB
+
+    multiplicar(2,2)
+    // importar função de outro arquivo
+
+    import {calcularIMC} from "./CalculadoraIMC"
+
+    // modo de import padrão do node
+    // const CalculadoraIMC = require("./CalculadoraIMC")
+
+    const res = calcularIMC(80,170)
+
+    console.log(res)
