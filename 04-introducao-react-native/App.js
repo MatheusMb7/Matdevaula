b6e695e6312d76fba7fd56f4714913763c2487c3
matheussbsiilva@gmail.com <matheussbsiilva@gmail.com>
2025-03-19 20:57:56 -0300

    atividade 04

--- 04-introducao-react-native/App.js
+++ 04-introducao-react-native/App.js
@@ -1,63 +1,99 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView, } from 'react-native';
 
 // principal componente
 // Ele deve retornar o que será renderizado na tela(templete feito com JSX)
 export default function App() {
 
   // Lógica do componente
-  const nome = "Aplicativo"
+  const nome = "Cristiano Ronaldo"
 
   function alerta() {
-    alert("Esse é meu aplicativo")
+    alert("Gol do palmeiras,  CRISTIANO RONALDO é o nome dele!!!")
   }
 
   // retorno com SX
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <View style={styles.container}>
     
 
-      {/* Comentario do template JSX */}
+    {/* Comentario do template JSX */}
 
-      {/* Còdigo Javascript */}
+    {/* Còdigo Javascript */}
 
-      <Text>{2 + 2}</Text>
-      <Text>{nome}</Text>
-
-      <Text style={style.textroGrande}>Palmeiras</Text>
-      <Text style={style.textroGrande}>Palmeiras</Text>
-      <Text>Palmeiras</Text>
-      <Text>Palmeiras</Text>
-      <Text>Palmeiras</Text>
     
+    <Text style={styles.textoGrande}>{nome}</Text>
+    <Text style={styles.textoGrande}>{3 + 4}</Text>
+    <Text>Cristiano Ronaldo dos Santos Aveiro é um futebolista português que atua como ponta-esquerda ou avançado.</Text>
+    <Text>Salário: 200 milhões EUR (2024)</Text>
+    <Text>Altura: 1,89 m</Text>
+    <Text>Peso: 83 kg</Text>
+  
 
-      <Button title='Clique' onPress={alerta}></Button>
+    <Button title='GOOL' onPress={alerta}></Button>
 
-      <Image
-         source={require('./https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSh3a7GOY1P8SXeFJHnYmUHp3rUPb4ncmnBNg&s')} 
-         style={{ 
-           height: 300,
-           width: 300,  
-        }}
-      />
+    <Image
+       source={{uri: 'https://i.pinimg.com/736x/fb/19/c4/fb19c40530191fd984f4e2b43593ddf4.jpg'}} 
+       style={{ 
+         height: 300,
+         width: 300,  
+      }}
+    />
+
+<Image
+       source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVbpyOnkhD2-tBCVJPrl6T7q3HiGXp3_SClQ&s'}} 
+       style={{ 
+         height: 300,
+         width: 300,  
+      }}
+    />
+
+    <Image
+    source={require('./imagens/image.png')}  
+    style={{
+      height: 300,
+      width: 300,
+    }}    
+
+    
+    />
+    <Image
+    source={require('./imagens/image2.png')}  
+    style={{
+      height: 300,
+      width: 300,
+    }}    
+    />
+    <Image
+    source={require('./imagens/image3.png')}  
+    style={{
+      height: 300,
+      width: 300,
+    }}    
+    />
 
 
-      <StatusBar style="auto" />
-    </View>
+
+    <StatusBar style="auto" />
+  </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#C8A2C8',
+    backgroundColor: '#3F7D58',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50,
+    fontStyle:'italic',
 
   },
   textoGrande: {
-    fontSize: 50,
+    fontSize: 30,
     fontWeight: 900,
     fontStyle: 'italic'
   }
