46a237373b4c260e2a3ade4e65696fb7eef67b6f
matheussbsiilva@gmail.com <matheussbsiilva@gmail.com>
2025-05-08 20:21:22 -0300

    adicioando 11 navegação

---
+++ 11-navegacao/App.js
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+
+import StackRoutes from './src/routes/StackRoutes';
+import TabRoutes from './src/routes/TabRoutes';
+import DrawerRoutes from './src/routes/DrawerRoutes';
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <StackRoutes />
+        <TabRoutes />
+        <DrawerRoutes />
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
